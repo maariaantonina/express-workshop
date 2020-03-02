8beaaba75c556f1ef4e7ff38911e77a209a51792
Maria Kasińska <mariakasinska17@gmail.com>
2020-03-02 22:02:39 +0100

    add handlebars

--- server.js
+++ server.js
@@ -1,47 +1,44 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use('/user', (req, res, next) => {
   res.send('You should log in to enter!');
 });
 
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
-
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.send('Hello' + req.params.id);
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
